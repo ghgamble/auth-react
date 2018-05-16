3bfa6c071185e60f039b60a35b3b52276d13acd0
Grace Gamble <grace.h.gamble@gmail.com>
2018-05-16 10:07:19 -0600

    Set Up Firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+      componentWillMount() {
+            firebase.initializeApp ({
+                  apiKey: "AIzaSyCN5c5C0IgIMNu5qTSTY5Eku5HPCatJaI8",
+                  authDomain: "auth-4c7ea.firebaseapp.com",
+                  databaseURL: "https://auth-4c7ea.firebaseio.com",
+                  projectId: "auth-4c7ea",
+                  storageBucket: "auth-4c7ea.appspot.com",
+                  messagingSenderId: "784080133550"
+            });
+      }
       render() {
             return (
                   <View>
